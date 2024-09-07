7278f047208431f27319bc6bc3fc12c0622ffba5
kinton <kintonkk@gmail.com>
2024-09-08 00:53:44 +0300

    Add MFO collection

---
+++ cms/src/api/mfo/routes/mfo.ts
@@ -0,0 +1,7 @@
+/**
+ * mfo router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::mfo.mfo');
---
+++ cms/src/api/mfo/services/mfo.ts
@@ -0,0 +1,7 @@
+/**
+ * mfo service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::mfo.mfo');
