d5ccdb259d3d07b96267b79706ac82e767782f24
kinton <kintonkk@gmail.com>
2024-09-18 22:08:59 +0300

    Install shacdnui

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: [ "class" ],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,21 +10,69 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: 'var(--color-primary)', // Используйте в классе: bg-primary для фона, text-primary для текста
-        // Используйте его для заголовков, кнопок и важных элементов. Этот цвет вызывает доверие и ассоциируется с надежностью.
-        secondary: 'var(--color-secondary)', // Используйте в классе: bg-secondary для фона, text-secondary для текста
-        // Подходит для акцентов, таких как кнопки "Получить займ" или "Узнать больше". Он символизирует рост и финансовое благополучие.
-        background: 'var(--color-background)', // Используйте в классе: bg-background для фона
-        // Используйте его для фона, чтобы создать чистый и современный вид, который не отвлекает от основного контента.
-        text: 'var(--color-text)', // Используйте в классе: text-text для цвета текста
-        // Этот цвет хорошо читается на светлом фоне и создает ощущение серьезности и профессионализма.
-        accent: 'var(--color-accent)', // Используйте в классе: bg-accent для фона, text-accent для текста
-        // Используйте его для выделения специальных предложений или акций. Он может побудить к действию, но не должен доминировать.
-        link: 'var(--color-link)', // Используйте в классе: text-link для цвета ссылок
-        // Более темный оттенок синего для ссылок, чтобы они выделялись, но оставались в рамках общей цветовой схемы.
+				primary: 'var(--color-primary)', // Используйте в классе: bg-primary для фона, text-primary для текста
+				// Используйте его для заголовков, кнопок и важных элементов. Этот цвет вызывает доверие и ассоциируется с надежностью.
+				secondary: 'var(--color-secondary)', // Используйте в классе: bg-secondary для фона, text-secondary для текста
+				// Подходит для акцентов, таких как кнопки "Получить займ" или "Узнать больше". Он символизирует рост и финансовое благополучие.
+				background: 'var(--color-background)', // Используйте в классе: bg-background для фона
+				// Используйте его для фона, чтобы создать чистый и современный вид, который не отвлекает от основного контента.
+				text: 'var(--color-text)', // Используйте в классе: text-text для цвета текста
+				// Этот цвет хорошо читается на светлом фоне и создает ощущение серьезности и профессионализма.
+				accent: 'var(--color-accent)', // Используйте в классе: bg-accent для фона, text-accent для текста
+				// Используйте его для выделения специальных предложений или акций. Он может побудить к действию, но не должен доминировать.
+				link: 'var(--color-link)', // Используйте в классе: text-link для цвета ссылок
+				// Более темный оттенок синего для ссылок, чтобы они выделялись, но оставались в рамках общей цветовой схемы.
+
+				// primary: {
+        //   DEFAULT: 'hsl(var(--primary))',
+        //   foreground: 'hsl(var(--primary-foreground))'
+        // },
+        // secondary: {
+        //   DEFAULT: 'hsl(var(--secondary))',
+        //   foreground: 'hsl(var(--secondary-foreground))'
+        // },
+        // background: 'hsl(var(--background))',
+        // text: 'var(--color-text)',
+        // accent: {
+        //   DEFAULT: 'hsl(var(--accent))',
+        //   foreground: 'hsl(var(--accent-foreground))'
+        // },
+        // link: 'var(--color-link)',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))'
+        }
       },
-    },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      }
+    }
   },
-  plugins: [],
+  plugins: [ require("tailwindcss-animate") ],
 };
 export default config;
