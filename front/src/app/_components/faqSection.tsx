73a68ef2746dab8920a5587e1e851d009f4ac506
kinton <kintonkk@gmail.com>
2024-10-10 01:09:43 +0300

    FR Setup prettier

--- front/src/app/_components/faqSection.tsx
+++ front/src/app/_components/faqSection.tsx
@@ -1,34 +1,39 @@
-"use client"
+"use client";
 
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
-import { getWebsiteInfo } from "../_queries/websiteInfo"
-import { useQuery } from "@tanstack/react-query"
-import { WebsiteInfoQuery } from "../_queries/gql/graphql"
+} from "@/components/ui/accordion";
+import { getWebsiteInfo } from "@/app/_queries/websiteInfo";
+import { useQuery } from "@tanstack/react-query";
+import { WebsiteInfoQuery } from "@/app/_queries/gql/graphql";
 
 export default function FAQSection() {
   const { data } = useQuery<WebsiteInfoQuery>({
-    queryKey: [ 'websiteInfo' ],
-    queryFn: getWebsiteInfo
-  })
+    queryKey: ["websiteInfo"],
+    queryFn: getWebsiteInfo,
+  });
 
-  const websiteInfo = data?.websiteInfo
+  const websiteInfo = data?.websiteInfo;
+  console.log(websiteInfo?.faq);
 
   return (
     <section className="py-12">
-      <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center text-primary">Часто задаваемые вопросы</h2>
+      <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center text-primary">
+        Часто задаваемые вопросы
+      </h2>
       <Accordion type="single" collapsible className="max-w-3xl mx-auto">
         {websiteInfo?.faq?.map((faq, index) => (
           <AccordionItem key={index} value={`item-${index}`}>
-            <AccordionTrigger className="text-left">{faq?.title}</AccordionTrigger>
+            <AccordionTrigger className="text-left">
+              {faq?.title}
+            </AccordionTrigger>
             <AccordionContent>{faq?.body}</AccordionContent>
           </AccordionItem>
         ))}
       </Accordion>
     </section>
-  )
+  );
 }
