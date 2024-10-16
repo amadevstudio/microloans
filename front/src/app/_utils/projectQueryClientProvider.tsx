8b9dc185e9c3e4754c0c78fcf432ac14bda4649d
kinton <kintonkk@gmail.com>
2024-10-17 00:35:31 +0300

    FR Use NEXT_PUBLIC_NODE_ENV in projectQueryClientProvider

--- front/src/app/_utils/projectQueryClientProvider.tsx
+++ front/src/app/_utils/projectQueryClientProvider.tsx
@@ -15,7 +15,8 @@ function makeQueryClient() {
       queries: {
         // With SSR, we usually want to set some default staleTime
         // above 0 to avoid refetching immediately on the client
-        staleTime: env.NODE_ENV === "development" ? 5 * 1000 : 60 * 1000,
+        staleTime:
+          env.NEXT_PUBLIC_NODE_ENV === "development" ? 5 * 1000 : 60 * 1000,
       },
     },
   });
