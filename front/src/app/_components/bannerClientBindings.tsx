7ad78bebb8f6a96ef264567f3b4a835e36f444bf
kinton <kintonkk@gmail.com>
2024-10-26 01:03:27 +0300

    FR Update scrollTo method

--- front/src/app/_components/bannerClientBindings.tsx
+++ front/src/app/_components/bannerClientBindings.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import { scrollToId } from "@/lib/utils/frontend/scrollTo";
@@ -8,18 +8,33 @@ import { ArrowRight, Search } from "lucide-react";
 export default function BannerClientBindings() {
   return (
     <>
-      <Link href="#filtersSection" onClick={e => scrollToId(e as unknown as MouseEvent, "filtersSection")}>
-        <Button className="cta-button text-white px-6 py-2 rounded-full w-full sm:w-auto" size="lg">
-          Начать поиск <Search className="ml-2 h-4 w-4"/>
+      <Link
+        href="#filtersSection"
+        onClick={(e) =>
+          scrollToId("filtersSection", { event: e as unknown as MouseEvent })
+        }
+      >
+        <Button
+          className="cta-button text-white px-6 py-2 rounded-full w-full sm:w-auto"
+          size="lg"
+        >
+          Начать поиск <Search className="ml-2 h-4 w-4" />
         </Button>
       </Link>
-      <Link href="#featuresSection" onClick={e => scrollToId(e as unknown as MouseEvent, "featuresSection")}>
-        <Button variant="outline"
-                className="bg-white text-primary hover:bg-primary hover:text-white border-white px-6 py-2 rounded-full w-full sm:w-auto"
-                size="lg">
-          Узнать больше <ArrowRight className="ml-2 h-4 w-4"/>
+      <Link
+        href="#featuresSection"
+        onClick={(e) =>
+          scrollToId("featuresSection", { event: e as unknown as MouseEvent })
+        }
+      >
+        <Button
+          variant="outline"
+          className="bg-white text-primary hover:bg-primary hover:text-white border-white px-6 py-2 rounded-full w-full sm:w-auto"
+          size="lg"
+        >
+          Узнать больше <ArrowRight className="ml-2 h-4 w-4" />
         </Button>
       </Link>
     </>
-  )
+  );
 }
--- front/src/lib/utils/frontend/scrollTo.ts
+++ front/src/lib/utils/frontend/scrollTo.ts
@@ -1,8 +1,11 @@
-export const scrollToId = (event: MouseEvent, id: string) => {
+export const scrollToId = (
+  id: string,
+  { event }: { event?: MouseEvent } = {},
+) => {
   const element = document.getElementById(id);
   if (element === null) return;
 
-  event.preventDefault();
+  event?.preventDefault();
   const offset = 100; // Adjust this value to scroll higher
   const top = element.getBoundingClientRect().top + window.scrollY - offset;
   window.scrollTo({ top, behavior: "smooth" });
