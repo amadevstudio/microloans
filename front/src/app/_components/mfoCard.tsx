6ee050231882e6e8a0000c360601e820147b9f4b
kinton <kintonkk@gmail.com>
2024-10-26 10:11:38 +0300

    FR Show full name with Info icon

--- front/src/app/_components/mfoCard.tsx
+++ front/src/app/_components/mfoCard.tsx
@@ -118,16 +118,14 @@ function Header({ mfo }: { mfo: Exclude<MfosQuery["mfos"][0], null> }) {
       </div>
       {mfo.rich_description ? (
         <div className="cursor-help">
-          <CardTitle className="text-primary text-base truncate flex gap-2">
-            {mfo.name} <Info className="w-4" />
+          <CardTitle className="text-primary text-base">
+            {mfo.name} <Info className="w-4 inline-block" />
           </CardTitle>
           <CardDescription>Предложение микрозайма (реклама)</CardDescription>
         </div>
       ) : (
         <div>
-          <CardTitle className="text-primary text-base truncate">
-            {mfo.name}
-          </CardTitle>
+          <CardTitle className="text-primary text-base">{mfo.name}</CardTitle>
           <CardDescription>Предложение микрозайма (реклама)</CardDescription>
         </div>
       )}
