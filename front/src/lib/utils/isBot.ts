7a158148e18a86197d1f9dbef46e672c57f49bd4
kinton <kintonkk@gmail.com>
2024-11-04 17:11:25 +0300

    FR Update isBot user agents list

--- front/src/lib/utils/isBot.ts
+++ front/src/lib/utils/isBot.ts
@@ -1,17 +1,28 @@
 export function isBot(userAgent: string) {
   const bots = [
-    /googlebot/i, // Googlebot
-    /pagespeed/i, // Google PageSpeed Insights
-    /bingbot/i, // Bingbot
-    /slackbot/i, // Slackbot
-    /facebookexternalhit/i, // Facebook External Hit
-    /twitterbot/i, // Twitterbot
-    /linkedinbot/i, // LinkedIn Bot
-    /yandexbot/i, // Yandex Bot
-    /crawler/i, // Generic crawler
-    /spider/i, // Generic spider
-    /robot/i, // Generic robot
-    /bot/i, // Generic bot
+    /\bgooglebot\b/i,
+    /Chrome-Lighthouse/i,
+    /\bbingbot\b/i,
+    /\bslackbot\b/i,
+    /facebookexternalhit/i,
+    /\btwitterbot\b/i,
+    /\blinkedinbot\b/i,
+    /\byandexbot\b/i,
+    /\bbingpreview\b/i, // Bing preview bot
+    /\bbaiduspider\b/i,
+    /\bduckduckbot\b/i,
+    /\bsogou\b/i,
+    /\bapplebot\b/i,
+    /\bsemrushbot\b/i,
+    /\bahrefsbot\b/i,
+    /\bmj12bot\b/i,
+    /\bpinterest\b/i,
+    /\buptimerobot\b/i,
+    /\bsite24x7\b/i,
+    /\bcrawler\b/i,
+    /\bspider\b/i,
+    /\brobot\b/i,
+    /\bbot\b/i,
   ];
 
   return bots.some((bot) => bot.test(userAgent));
